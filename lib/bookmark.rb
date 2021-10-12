118c3423f60c0eaeffdf15075ffe6eee1c2a8ee0
Troy Snowden <troybradsnowden@gmail.com>
2021-10-12 15:42:10 +0100

    Create table of bookmarks on front end

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -6,12 +6,12 @@ class Bookmark
 
   def self.all
     bookmarks_arr = []
-    con = PG.connect dbname: 'bookmark_manager', user: 'tombaxter'
+    con = PG.connect dbname: 'bookmark_manager', user: ENV['USER']
 
     rs = con.exec 'SELECT * FROM bookmarks'
 
     rs.each do |row|
-      bookmarks_arr << "#{row['id']}. #{row['url']}"
+      bookmarks_arr << "#{row['id']},#{row['url']}"
     end
     bookmarks_arr
   rescue PG::Error => e
